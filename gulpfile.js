6cbbc0598ac038f92af8920d7a404fcc6e8f44be
Jack Hughes <hughes_jack@outlook.com>
2018-02-14 02:36:22 +0000

    Fixed issue with Browser-Sync - now functions normally

--- gulpfile.js
+++ gulpfile.js
@@ -1,33 +1,46 @@
 var gulp = require('gulp'),
-watch = require('gulp-watch'),
 postcss = require('gulp-postcss'),
 autoprefixer = require('autoprefixer'),
 cssvars = require('postcss-simple-vars'),
 nested = require('postcss-nested'),
-cssImport = require('postcss-import');
-
-gulp.task('default', function() {
-	console.log("placeholder");
-});
-
-gulp.task('html', function() {
-	console.log("placeholder");
-});
+cssImport = require('postcss-import'),
+mixins = require('postcss-mixins'),
+watch = require('gulp-watch'),
+browserSync = require('browser-sync').create();
 
 gulp.task('styles', function() {
-	return gulp.src('./site/assets/styles/styles.css')
-	.pipe(postcss([cssImport, nested, cssvars, autoprefixer]))
-	.pipe(gulp.dest('./site/prod/styles'));
+  return gulp.src('./site/assets/styles/styles.css')
+  .pipe(postcss([cssImport, mixins, cssvars, nested, autoprefixer]))
+  .on('error', function(errorInfo) {
+  	console.log(errorInfo.toString());
+  	this.emit('end');
+	})
+  .pipe(gulp.dest('./site/prod/styles'));
 });
 
 gulp.task('watch', function() {
-
-	watch('./site/index.html', function() {
-		gulp.start('html');
+	browserSync.init({
+		notify: false,
+		server: {
+			baseDir: "./"
+	}
 	});
 
-	watch('./site/assets/styles/*/*.css', function() {
-		gulp.start('styles');
+	watch('./index.html', function() {
+		gulp.start('HTMLBrowserRefresh');
 	});
-	
+
+	watch('./site/assets/styles/**/*.css', function() {
+		gulp.start('CSSBrowserInject');
+	});
 });
+
+gulp.task('CSSBrowserInject', ['styles'], function() {
+	return gulp.src('./site/prod/styles/styles.css')
+		.pipe(browserSync.stream());
+});
+
+gulp.task('HTMLBrowserRefresh', function() {
+	browserSync.reload();
+});
+	
